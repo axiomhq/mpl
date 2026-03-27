00df298bdd88c8bd8a9480d8a1c1fc5bb2e4a399
Heinz N. Gies <heinz@licenser.net>
2026-03-27 16:13:13 +0100

    Expose query walker and visitor
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- src/visitor.rs
+++ src/visitor.rs
@@ -10,35 +10,45 @@ use crate::{
     types::{Dataset, Metric, Parameterized},
 };
 
+/// Result of a visit operation.
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
-pub(crate) enum VisitRes {
+pub enum VisitRes {
+    /// Continue walking.
     Walk,
+    /// Stop walking.
     Stop,
 }
 
-pub(crate) trait QueryVisitor {
+/// Visitor for all query components.
+pub trait QueryVisitor {
+    /// Error type for the visitor.
     type Error: std::error::Error;
 
+    /// Visit a query.
     fn visit(&mut self, query: &mut Query) -> Result<VisitRes, Self::Error> {
         let _ = query;
         Ok(VisitRes::Walk)
     }
 
+    /// Leave a query.
     fn leave(&mut self, query: &mut Query) -> Result<(), Self::Error> {
         let _ = query;
         Ok(())
     }
 
+    /// Visit a source.
     fn visit_source(&mut self, source: &mut Source) -> Result<VisitRes, Self::Error> {
         let _ = source;
         Ok(VisitRes::Walk)
     }
 
+    /// Leave a source.
     fn leave_source(&mut self, source: &mut Source) -> Result<(), Self::Error> {
         let _ = source;
         Ok(())
     }
 
+    /// Visit a dataset.
     fn visit_dataset(
         &mut self,
         dataset: &mut Parameterized<Dataset>,
@@ -47,73 +57,87 @@ pub(crate) trait QueryVisitor {
         Ok(VisitRes::Walk)
     }
 
+    /// Leave a dataset.
     fn leave_dataset(&mut self, dataset: &mut Parameterized<Dataset>) -> Result<(), Self::Error> {
         let _ = dataset;
         Ok(())
     }
 
+    /// Visit a metric.
     fn visit_metric(&mut self, metric: &mut Metric) -> Result<VisitRes, Self::Error> {
         let _ = metric;
         Ok(VisitRes::Walk)
     }
 
+    /// Leave a metric.
     fn leave_metric(&mut self, metric: &mut Metric) -> Result<(), Self::Error> {
         let _ = metric;
         Ok(())
     }
 
+    /// Visit a sample.
     fn visit_sample(&mut self, sample: &mut Option<f64>) -> Result<VisitRes, Self::Error> {
         let _ = sample;
         Ok(VisitRes::Walk)
     }
 
+    /// Leave a sample.
     fn leave_sample(&mut self, sample: &mut Option<f64>) -> Result<(), Self::Error> {
         let _ = sample;
         Ok(())
     }
 
+    /// Visit filters.
     fn visit_filters(&mut self, filters: &mut Vec<Filter>) -> Result<VisitRes, Self::Error> {
         let _ = filters;
         Ok(VisitRes::Walk)
     }
 
+    /// Leave filters.
     fn leave_filters(&mut self, filters: &mut Vec<Filter>) -> Result<(), Self::Error> {
         let _ = filters;
         Ok(())
     }
 
+    /// Visit a filter.
     fn visit_filter(&mut self, filter: &mut Filter) -> Result<VisitRes, Self::Error> {
         let _ = filter;
         Ok(VisitRes::Walk)
     }
 
+    /// Leave a filter.
     fn leave_filter(&mut self, filter: &mut Filter) -> Result<(), Self::Error> {
         let _ = filter;
         Ok(())
     }
 
+    /// Visit a comparison.
     fn visit_cmp(&mut self, field: &mut String, cmp: &mut Cmp) -> Result<VisitRes, Self::Error> {
         let _ = field;
         let _ = cmp;
         Ok(VisitRes::Walk)
     }
 
+    /// Leave a comparison.
     fn leave_cmp(&mut self, field: &mut String, cmp: &mut Cmp) -> Result<(), Self::Error> {
         let _ = field;
         let _ = cmp;
         Ok(())
     }
 
+    /// Visit an operation.
     fn visit_op(&mut self, op: &mut ComputeFunction) -> Result<VisitRes, Self::Error> {
         let _ = op;
         Ok(VisitRes::Walk)
     }
 
+    /// Leave an operation.
     fn leave_op(&mut self, op: &mut ComputeFunction) -> Result<(), Self::Error> {
         let _ = op;
         Ok(())
     }
 
+    /// Visit aggregates.
     fn visit_aggregates(
         &mut self,
         aggregates: &mut Vec<Aggregate>,
@@ -122,81 +146,97 @@ pub(crate) trait QueryVisitor {
         Ok(VisitRes::Walk)
     }
 
+    /// Leave aggregates.
     fn leave_aggregates(&mut self, aggregates: &mut Vec<Aggregate>) -> Result<(), Self::Error> {
         let _ = aggregates;
         Ok(())
     }
 
+    /// Visit an aggregate.
     fn visit_aggregate(&mut self, aggregate: &mut Aggregate) -> Result<VisitRes, Self::Error> {
         let _ = aggregate;
         Ok(VisitRes::Walk)
     }
 
+    /// Leave an aggregate.
     fn leave_aggregate(&mut self, aggregate: &mut Aggregate) -> Result<(), Self::Error> {
         let _ = aggregate;
         Ok(())
     }
 
+    /// Visit a mapping.
     fn visit_mapping(&mut self, mapping: &mut Mapping) -> Result<VisitRes, Self::Error> {
         let _ = mapping;
         Ok(VisitRes::Walk)
     }
 
+    /// Leave a mapping.
     fn leave_mapping(&mut self, mapping: &mut Mapping) -> Result<(), Self::Error> {
         let _ = mapping;
         Ok(())
     }
 
+    /// Visit an align.
     fn visit_align(&mut self, align: &mut Align) -> Result<VisitRes, Self::Error> {
         let _ = align;
         Ok(VisitRes::Walk)
     }
 
+    /// Leave an align.
     fn leave_align(&mut self, align: &mut Align) -> Result<(), Self::Error> {
         let _ = align;
         Ok(())
     }
 
+    /// Visit a group by.
     fn visit_group_by(&mut self, group_by: &mut GroupBy) -> Result<VisitRes, Self::Error> {
         let _ = group_by;
         Ok(VisitRes::Walk)
     }
 
+    /// Leave a group by.
     fn leave_group_by(&mut self, group_by: &mut GroupBy) -> Result<(), Self::Error> {
         let _ = group_by;
         Ok(())
     }
 
+    /// Visit a bucket by.
     fn visit_bucket_by(&mut self, bucket_by: &mut BucketBy) -> Result<VisitRes, Self::Error> {
         let _ = bucket_by;
         Ok(VisitRes::Walk)
     }
 
+    /// Leave a bucket by.
     fn leave_bucket_by(&mut self, bucket_by: &mut BucketBy) -> Result<(), Self::Error> {
         let _ = bucket_by;
         Ok(())
     }
 
+    /// Visit an as.
     fn visit_as(&mut self, as_: &mut As) -> Result<VisitRes, Self::Error> {
         let _ = as_;
         Ok(VisitRes::Walk)
     }
 
+    /// Leave an as.
     fn leave_as(&mut self, as_: &mut As) -> Result<(), Self::Error> {
         let _ = as_;
         Ok(())
     }
 
+    /// Visit directives.
     fn visit_directives(&mut self, directives: &mut Directives) -> Result<VisitRes, Self::Error> {
         let _ = directives;
         Ok(VisitRes::Walk)
     }
 
+    /// Leave directives.
     fn leave_directives(&mut self, directives: &mut Directives) -> Result<(), Self::Error> {
         let _ = directives;
         Ok(())
     }
 
+    /// Visit a directive.
     fn visit_directive(
         &mut self,
         name: &String,
@@ -207,6 +247,7 @@ pub(crate) trait QueryVisitor {
         Ok(VisitRes::Walk)
     }
 
+    /// Leave a directive.
     fn leave_directive(
         &mut self,
         name: &String,
@@ -217,21 +258,25 @@ pub(crate) trait QueryVisitor {
         Ok(())
     }
 
+    /// Visit params.
     fn visit_params(&mut self, params: &mut Vec<Param>) -> Result<VisitRes, Self::Error> {
         let _ = params;
         Ok(VisitRes::Walk)
     }
 
+    /// Leave params.
     fn leave_params(&mut self, params: &mut Vec<Param>) -> Result<(), Self::Error> {
         let _ = params;
         Ok(())
     }
 
+    /// Visit a param.
     fn visit_param(&mut self, param: &mut Param) -> Result<VisitRes, Self::Error> {
         let _ = param;
         Ok(VisitRes::Walk)
     }
 
+    /// Leave a param.
     fn leave_param(&mut self, param: &mut Param) -> Result<(), Self::Error> {
         let _ = param;
         Ok(())
@@ -246,7 +291,9 @@ macro_rules! stop {
     };
 }
 
-pub(crate) trait QueryWalker: QueryVisitor {
+/// A trait for walking a query.
+pub trait QueryWalker: QueryVisitor {
+    /// Walk a query.
     fn walk(&mut self, query: &mut Query) -> Result<(), Self::Error> {
         stop!(
             QueryVisitor::visit(self, query),
@@ -290,6 +337,7 @@ pub(crate) trait QueryWalker: QueryVisitor {
         QueryVisitor::leave(self, query)
     }
 
+    /// Walk a source.
     fn walk_source(&mut self, source: &mut Source) -> Result<(), Self::Error> {
         stop!(
             QueryVisitor::visit_source(self, source),
@@ -301,23 +349,28 @@ pub(crate) trait QueryWalker: QueryVisitor {
         Ok(())
     }
 
+    /// Walk a dataset.
     fn walk_dataset(&mut self, dataset: &mut Parameterized<Dataset>) -> Result<(), Self::Error> {
         QueryVisitor::visit_dataset(self, dataset)?;
         QueryVisitor::leave_dataset(self, dataset)?;
         Ok(())
     }
 
+    /// Walk a metric.
     fn walk_metric(&mut self, metric: &mut Metric) -> Result<(), Self::Error> {
         QueryVisitor::visit_metric(self, metric)?;
         QueryVisitor::leave_metric(self, metric)?;
         Ok(())
     }
+
+    /// Walk a sample.
     fn walk_sample(&mut self, sample: &mut Option<f64>) -> Result<(), Self::Error> {
         QueryVisitor::visit_sample(self, sample)?;
         QueryVisitor::leave_sample(self, sample)?;
         Ok(())
     }
 
+    /// Walk filters.
     fn walk_filters(&mut self, filters: &mut Vec<Filter>) -> Result<(), Self::Error> {
         stop!(
             QueryVisitor::visit_filters(self, filters),
@@ -330,6 +383,7 @@ pub(crate) trait QueryWalker: QueryVisitor {
         Ok(())
     }
 
+    /// Walk a filter.
     fn walk_filter(&mut self, filter: &mut Filter) -> Result<(), Self::Error> {
         stop!(
             QueryVisitor::visit_filter(self, filter),
@@ -348,18 +402,21 @@ pub(crate) trait QueryWalker: QueryVisitor {
         Ok(())
     }
 
+    /// Walk a cmp.
     fn walk_cmp(&mut self, field: &mut String, rhs: &mut Cmp) -> Result<(), Self::Error> {
         QueryVisitor::visit_cmp(self, field, rhs)?;
         QueryVisitor::leave_cmp(self, field, rhs)?;
         Ok(())
     }
 
+    /// Walk an op.
     fn walk_op(&mut self, op: &mut ComputeFunction) -> Result<(), Self::Error> {
         QueryVisitor::visit_op(self, op)?;
         QueryVisitor::leave_op(self, op)?;
         Ok(())
     }
 
+    /// Walk aggregates.
     fn walk_aggregates(&mut self, aggregates: &mut Vec<Aggregate>) -> Result<(), Self::Error> {
         stop!(
             QueryVisitor::visit_aggregates(self, aggregates),
@@ -372,6 +429,7 @@ pub(crate) trait QueryWalker: QueryVisitor {
         Ok(())
     }
 
+    /// Walk an aggregate.
     fn walk_aggregate(&mut self, aggregate: &mut Aggregate) -> Result<(), Self::Error> {
         stop!(
             QueryVisitor::visit_aggregate(self, aggregate),
@@ -388,30 +446,35 @@ pub(crate) trait QueryWalker: QueryVisitor {
         Ok(())
     }
 
+    /// Walk a mapping.
     fn walk_mapping(&mut self, mapping: &mut Mapping) -> Result<(), Self::Error> {
         QueryVisitor::visit_mapping(self, mapping)?;
         QueryVisitor::leave_mapping(self, mapping)?;
         Ok(())
     }
 
+    /// Walk an align.
     fn walk_align(&mut self, align: &mut Align) -> Result<(), Self::Error> {
         QueryVisitor::visit_align(self, align)?;
         QueryVisitor::leave_align(self, align)?;
         Ok(())
     }
 
+    /// Walk a group by.
     fn walk_group_by(&mut self, group_by: &mut GroupBy) -> Result<(), Self::Error> {
         QueryVisitor::visit_group_by(self, group_by)?;
         QueryVisitor::leave_group_by(self, group_by)?;
         Ok(())
     }
 
+    /// Walk a bucket by.
     fn walk_bucket_by(&mut self, bucket_by: &mut BucketBy) -> Result<(), Self::Error> {
         QueryVisitor::visit_bucket_by(self, bucket_by)?;
         QueryVisitor::leave_bucket_by(self, bucket_by)?;
         Ok(())
     }
 
+    /// Walk an as.
     fn walk_as(&mut self, as_: &mut As) -> Result<(), Self::Error> {
         QueryVisitor::visit_as(self, as_)?;
         QueryVisitor::visit_metric(self, &mut as_.name)?;
@@ -419,6 +482,7 @@ pub(crate) trait QueryWalker: QueryVisitor {
         Ok(())
     }
 
+    /// Walk directives.
     fn walk_directives(&mut self, directives: &mut Directives) -> Result<(), Self::Error> {
         stop!(
             QueryVisitor::visit_directives(self, directives),
@@ -431,6 +495,7 @@ pub(crate) trait QueryWalker: QueryVisitor {
         Ok(())
     }
 
+    /// Walk a directive.
     fn walk_directive(
         &mut self,
         name: &String,
@@ -441,6 +506,7 @@ pub(crate) trait QueryWalker: QueryVisitor {
         Ok(())
     }
 
+    /// Walk params.
     fn walk_params(&mut self, params: &mut Vec<Param>) -> Result<(), Self::Error> {
         stop!(
             QueryVisitor::visit_params(self, params),
@@ -453,6 +519,7 @@ pub(crate) trait QueryWalker: QueryVisitor {
         Ok(())
     }
 
+    /// Walk a param.
     fn walk_param(&mut self, param: &mut Param) -> Result<(), Self::Error> {
         QueryVisitor::visit_param(self, param)?;
         QueryVisitor::leave_param(self, param)?;
